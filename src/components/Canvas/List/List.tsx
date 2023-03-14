26fcf9073e84217f649e0613f99c5fe2456d4dbc
Timur Gamkrelidze <timur.gamkrelidze@bravedevelopers.com>
2023-03-14 21:38:30 +0400

    fix: styles

--- src/components/Canvas/List/List.tsx
+++ src/components/Canvas/List/List.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './List.module.scss'
 import { useAppSelector } from '../../../hooks'
 import { CanvasComponent } from './CanvasComponent/CanvasComponent'
+import { Line } from '../../shared/Line'
 
 export const List = () => {
+  const [isOverEmptyPlace, setIsOverEmptyPlace] = useState(false)
   const components = useAppSelector(store => store.canvasConstructor.canvas)
 
   return (
@@ -11,6 +13,14 @@ export const List = () => {
       {components.map(comp =>
         <CanvasComponent key={comp} component={comp} />
       )}
+      <div
+        className={styles.emptyPlace}
+        onDragEnter={() => { setIsOverEmptyPlace(true) }}
+        onDragLeave={() => { setIsOverEmptyPlace(false) }}
+        onDrop={() => { setIsOverEmptyPlace(false) }}
+      >
+        {isOverEmptyPlace && <Line />}
+      </div>
     </div>
   )
 }
